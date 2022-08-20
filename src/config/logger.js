a35ea22fdce1dc5ceba090da1dc1ceef3fd61634
Marcos Lima <arthur.lima.dev@gmail.com>
2022-08-20 19:22:03 -0300

    update docker; disposability

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,11 +1,11 @@
-const winston = require('winston');
+const winston = require('winston')
+const ecsFormat = require('@elastic/ecs-winston-format')
 
-const day = new Date().toJSON().slice(0,10);
 const logger = winston.createLogger({
-    transports: [
-      new winston.transports.Console(),
-      new winston.transports.File({ filename: `logs/twelve-factor_${day}.log` })
-    ]
-});
+  format: ecsFormat(), 
+  transports: [
+    new winston.transports.Console()
+  ]
+})
 
 module.exports = logger
